ed859bf1652274ffa32f4d3da3aba81c60151734
nitin786-sudo <nitin.nitesh24@gmail.com>
2026-03-13 18:07:47 +0530

    addedstg

--- code/stg/main.tf
+++ code/stg/main.tf
@@ -6,3 +6,11 @@ resource "azurerm_storage_account" "stg" {
     account_replication_type = "LRS"
 }
 
+resource "azurerm_storage_account" "stg" {
+    name = "stgprod12345"
+    location = "eastus"
+    resource_group_name = "mahatma_gandhi"
+    account_tier = "Standard"
+    account_replication_type = "LRS"
+}
+
