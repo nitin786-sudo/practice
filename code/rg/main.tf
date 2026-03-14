a2826d5cb2f06a148452ec4922a25d1153971471
nitin786-sudo <nitin.nitesh24@gmail.com>
2026-03-14 10:15:03 +0530

    addedkuchbhi

--- code/rg/main.tf
+++ code/rg/main.tf
@@ -7,3 +7,8 @@ resource "azurerm_resource_group" "rg2" {
     name = "mahatma_gandhiee"
     location = "eastus"
 }
+
+resource "azurerm_resource_group" "rg3" {
+    name = "mahatma_gdhiee"
+    location = "eastus"
+}
